966faedb01f105d7e890b52465572bfd04d4713f
3553x <3553x@tuta.io>
2018-11-19 11:34:24 +0000

    Add comments to header file

--- aes-avr.h
+++ aes-avr.h
@@ -1,9 +1,18 @@
 extern "C" {
+	//performs key expansion
 
-	void expand_key(struct data, void *expanded);
+	//key is the key to expand in binary format
+	//expanded points to a 176 byte array to hold the expanded key
+	void expand_key(struct data key, void *expanded);
+
+	//transforms an expanded key to a key for decryption
+	
+	//expanded points to a 176 byte array to hold the expanded key
 	void decrypt_key(void *expanded);
-	struct data encrypt_data(struct data, void *expanded);
-	struct data decrypt_data(struct data, void *expanded);
+	
+
+	struct data encrypt_data(struct data plaintext, void *expanded);
+	struct data decrypt_data(struct data ciphertext, void *expanded);
 
 	struct data{
 	  unsigned char data[16];
